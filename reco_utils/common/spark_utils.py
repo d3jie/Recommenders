31a2d18144e890e6a18b6238e37c396ab5129af0
Scott Graham <gramhagen@users.noreply.github.com>
2019-03-12 13:53:03 -0400

    adding env var option to start_or_get_spark

--- reco_utils/common/spark_utils.py
+++ reco_utils/common/spark_utils.py
@@ -1,23 +1,30 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+import os
+
+
 try:
     from pyspark.sql import SparkSession
 except ImportError:
-    pass  # skip this import if we are in pure python environment
+    SparkSession = None  # skip this import if we are in pure python environment
 
 
-def start_or_get_spark(app_name="Sample", url="local[*]", memory="10G"):
+def start_or_get_spark(app_name="Sample", url="local[*]", memory="10G", env_vars=None):
     """Start Spark if not started
 
     Args:
         app_name (str): Set name of the application
-        url (str): URL for spark master.
-        memory (str): Size of memory for spark driver.
-        
+        url (str): URL for spark master
+        memory (str): Size of memory for spark driver
+        env_vars (dict): optional kwargs to use for setting environment variables
     Returns:
         obj: Spark context.
     """
+
+    if env_vars is not None:
+        os.environ.update(env_vars)
+
     spark = (
         SparkSession.builder.appName(app_name)
         .master(url)
